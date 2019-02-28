273f5847509a32b6d7b14fd72e87f9d42e7210cf
shohi <oshohi@gmail.com>
2019-02-28 17:10:40 +0800

    update nats/nats-streaming Dockerfiles - populate git commit info when building

--- nats-streaming/resources/binary_build.sh
+++ nats-streaming/resources/binary_build.sh
@@ -2,11 +2,22 @@
 
 GOBIN=$GOPATH/bin
 
-go get github.com/nats-io/nats-streaming-server
-cd $GOPATH/src/github.com/nats-io/nats-streaming-server
+# go get github.com/nats-io/nats-streaming-server
+# cd $GOPATH/src/github.com/nats-io/nats-streaming-server
+mkdir -p $GOPATH/src/github.com/nats-io
+cd $GOPATH/src/github.com/nats-io
+git clone https://github.com/nats-io/nats-streaming-server.git
+cd nats-streaming-server
 
 git pull --all
-git checkout 7e7bd7a -b v0.12.1
-go build -o nats-streaming-server nats-streaming-server.go
+git checkout 5baec0d -b v0.12.1
 
-cp nats-streaming-server /app/resources/nats-streaming-server.v0.12.1
+CGO_ENABLED=0 go install -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`"
+
+# CGO_ENABLED=0 GOOS=linux   GOARCH=amd64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/linux-amd64/nats-streaming-server
+# CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=6 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/linux-arm6/nats-streaming-server
+# CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=7 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/linux-arm7/nats-streaming-server
+# CGO_ENABLED=0 GOOS=linux   GOARCH=arm64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/linux-arm64/nats-streaming-server
+# CGO_ENABLED=0 GOOS=windows GOARCH=amd64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/win-amd64/nats-streaming-server.exe
+
+cp $GOBIN/nats-streaming-server /app/resources/nats-streaming-server.v0.12.1
---
+++ nats/resources/binary_build.sh
@@ -0,0 +1,25 @@
+#! /bin/sh -x
+
+GOBIN=$GOPATH/bin
+
+# go get github.com/nats-io/gnatsd
+# cd $GOPATH/src/github.com/nats-io/gnatsd
+mkdir -p $GOPATH/src/github.com/nats-io
+cd $GOPATH/src/github.com/nats-io
+git clone https://github.com/nats-io/gnatsd.git
+cd gnatsd
+
+git pull --all
+git checkout tags/v1.4.1 -b v1.4.1
+
+CGO_ENABLED=0 go install -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`"
+
+# CGO_ENABLED=0 GOOS=linux   GOARCH=amd64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-amd64/gnatsd
+# CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=6 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm6/gnatsd
+# CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=7 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm7/gnatsd
+# CGO_ENABLED=0 GOOS=linux   GOARCH=arm64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm64/gnatsd
+# CGO_ENABLED=0 GOOS=windows GOARCH=amd64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/win-amd64/gnatsd
+
+# cp $GOPATH/pkg/linux-amd64/nats-streaming-server /app/resources/nats.v1.4.1
+
+cp $GOBIN/gnatsd /app/resources/gnatsd.v1.4.1
