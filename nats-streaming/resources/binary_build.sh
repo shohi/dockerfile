f762fb38c061fcbd0fcd3e5a74480d05f3318cee
shohi <oshohi@gmail.com>
2019-03-11 17:37:56 +0800

    update Dockerfiles for nats and nats-streaming

--- nats-streaming/resources/binary_build.sh
+++ nats-streaming/resources/binary_build.sh
@@ -12,12 +12,16 @@ cd nats-streaming-server
 git pull --all
 git checkout 5baec0d -b v0.12.1
 
-CGO_ENABLED=0 go install -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`"
+# CGO_ENABLED=0 go install -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`"
+CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=6 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm6/nats-streaming-server
+CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=7 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm7/nats-streaming-server
+CGO_ENABLED=0 GOOS=linux   GOARCH=arm64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm64/nats-streaming-server
 
-# CGO_ENABLED=0 GOOS=linux   GOARCH=amd64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/linux-amd64/nats-streaming-server
-# CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=6 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/linux-arm6/nats-streaming-server
-# CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=7 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/linux-arm7/nats-streaming-server
-# CGO_ENABLED=0 GOOS=linux   GOARCH=arm64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/linux-arm64/nats-streaming-server
-# CGO_ENABLED=0 GOOS=windows GOARCH=amd64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o pkg/win-amd64/nats-streaming-server.exe
+CGO_ENABLED=0 GOOS=linux   GOARCH=amd64  go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-amd64/nats-streaming-server
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64  go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/nats-streaming-server/version.GITCOMMIT=`git rev-parse --short HEAD`" -o $GOPATH/pkg/pkg/win-amd64/nats-streaming-server.exe
+CGO_ENABLED=0 GOOS=darwin  GOARCH=amd64  go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/darwin-amd64/nats-streaming-server
 
-cp $GOBIN/nats-streaming-server /app/resources/nats-streaming-server.v0.12.1
+cp $GOPATH/pkg/linux-amd64/nats-streaming-server /app/resources/nats-streaming-server.v0.12.1
+cp $GOPATH/pkg/darwin-amd64/nats-streaming-server /app/resources/nats-streaming-server-darwin.v0.12.1
+
+# cp $GOBIN/nats-streaming-server /app/resources/nats-streaming-server.v0.12.1
--- nats/resources/binary_build.sh
+++ nats/resources/binary_build.sh
@@ -12,14 +12,17 @@ cd gnatsd
 git pull --all
 git checkout tags/v1.4.1 -b v1.4.1
 
-CGO_ENABLED=0 go install -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`"
+#CGO_ENABLED=0 go install -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`"
 
-# CGO_ENABLED=0 GOOS=linux   GOARCH=amd64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-amd64/gnatsd
-# CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=6 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm6/gnatsd
-# CGO_ENABLED=0 GOOS=linux   GOARCH=arm   GOARM=7 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm7/gnatsd
-# CGO_ENABLED=0 GOOS=linux   GOARCH=arm64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm64/gnatsd
-# CGO_ENABLED=0 GOOS=windows GOARCH=amd64         go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/win-amd64/gnatsd
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=6 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm6/gnatsd
+CGO_ENABLED=0 GOOS=linux GOARCH=arm GOARM=7 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm7/gnatsd
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-arm64/gnatsd
 
-# cp $GOPATH/pkg/linux-amd64/nats-streaming-server /app/resources/nats.v1.4.1
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/linux-amd64/gnatsd
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/win-amd64/gnatsd
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -v -a -tags netgo -installsuffix netgo -ldflags "-s -w -X github.com/nats-io/gnatsd/server.gitCommit=`git rev-parse --short HEAD`" -o $GOPATH/pkg/darwin-amd64/gnatsd
 
-cp $GOBIN/gnatsd /app/resources/gnatsd.v1.4.1
+cp $GOPATH/pkg/linux-amd64/gnatsd /app/resources/gnatsd-linux.v1.4.1
+cp $GOPATH/pkg/darwin-amd64/gnatsd /app/resources/gnatsd-darwin.v1.4.1
+
+# cp $GOBIN/gnatsd /app/resources/gnatsd.v1.4.1
