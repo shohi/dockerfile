ef2d88d0d565eb697171469977ba02788f800f31
shohi <oshohi@gmail.com>
2019-06-14 19:23:25 +0800

    update golang-graphviz

--- golang-graphviz/build.sh
+++ golang-graphviz/build.sh
@@ -1,7 +1,14 @@
 #! /bin/bash -x
 start=`date +%s`
 
+GO_VERSION=1.12.5
+
 docker build -t golang:graphviz .
+docker build \
+    -t golang:graphviz \
+    --build-arg GO_VERSION=${GO_VERSION} \
+    .
+
 
 end=`date +%s`
 runtime=$((end-start))
